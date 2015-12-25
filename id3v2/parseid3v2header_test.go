3aa1eddeadda879b0e74b5bdef69b5b7d2f0ccc4
Andrey <andreykvital@gmail.com>
2015-12-25 20:42:36 -0200

    More tests

--- id3v2/parseid3v2header_test.go
+++ id3v2/parseid3v2header_test.go
@@ -41,20 +41,31 @@ func TestParseUnrecognizedID3MajorVersion(t *testing.T) {
 	assert.Equal(t, id3v2.ErrUnrecognizedVersion, err)
 }
 
-func TestParseID3v2Header(t *testing.T) {
-	reader := bytes.NewReader([]byte{
-		73, 68, 51, 3, 0,
-		0, 0, 6, 96, 117,
-	})
+func TestParseInvalidSynchSafeSize(t *testing.T) {
+	reader := bytes.NewReader([]byte{73, 68, 51, 3, 0, 0, 128, 128, 128, 128})
 
 	header, err := id3v2.ParseID3v2Header(reader)
 
-	assert.Nil(t, err)
-	assert.Equal(t, 3, header.MajorVersion)
-	assert.Equal(t, 0, header.MinorVersion)
-	assert.False(t, header.Unsynchronization)
-	assert.False(t, header.Extended)
-	assert.False(t, header.Experimental)
-	assert.False(t, header.Footer)
-	assert.Equal(t, uint64(111479), header.Size)
+	assert.Nil(t, header)
+	assert.Equal(t, id3v2.ErrInvalidSize, err)
+}
+
+func TestParseID3v2Header(t *testing.T) {
+	for _, headerBytes := range [][]byte{
+		[]byte{73, 68, 51, 2, 0, 0, 0, 6, 96, 117},
+		[]byte{73, 68, 51, 3, 0, 0, 0, 6, 96, 117},
+		[]byte{73, 68, 51, 4, 0, 0, 0, 6, 96, 117},
+	} {
+		reader := bytes.NewReader(headerBytes)
+		header, err := id3v2.ParseID3v2Header(reader)
+
+		assert.Nil(t, err)
+		assert.Equal(t, int(headerBytes[3]), header.MajorVersion)
+		assert.Equal(t, 0, header.MinorVersion)
+		assert.False(t, header.Unsynchronization)
+		assert.False(t, header.Extended)
+		assert.False(t, header.Experimental)
+		assert.False(t, header.Footer)
+		assert.Equal(t, uint64(111479), header.Size)
+	}
 }
