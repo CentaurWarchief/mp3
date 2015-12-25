d6fc11ff69dc20ac864f22d05a24796c5ce00ecd
Andrey <andreykvital@gmail.com>
2015-12-25 18:42:27 -0200

    Update message

--- id3v2/errors.go
+++ id3v2/errors.go
@@ -5,6 +5,6 @@ import "errors"
 var (
 	ErrInvalidSize         = errors.New("invalid size")
 	ErrID3HeaderNotFound   = errors.New("ID3 header not found")
-	ErrUnrecognizedVersion = errors.New("unrecognized ID3v2 version")
-	ErrUnsupportedVersion  = errors.New("unsupported ID3v2 version")
+	ErrUnrecognizedVersion = errors.New("unrecognized version")
+	ErrUnsupportedVersion  = errors.New("unsupported version")
 )
