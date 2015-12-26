0750103db845f22fbe449e5b79a728d5e95bb8f9
Andrey <andreykvital@gmail.com>
2015-12-25 22:45:59 -0200

    Remove old comment

--- id3v2/id3v2taglist.go
+++ id3v2/id3v2taglist.go
@@ -3,10 +3,6 @@ package id3v2
 type ID3v2TagList map[string]string
 
 func (l ID3v2TagList) HasTag(tag string) bool {
-	// if len(tag) != 4 && len(tag) != 3 {
-	// 	return false
-	// }
-
 	if _, ok := l[tag]; ok {
 		return true
 	}
