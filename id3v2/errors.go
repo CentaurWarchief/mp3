b473ba61cd04b52e6fcd46cf3406655f17dcaff5
Andrey <andreykvital@gmail.com>
2015-12-27 16:08:37 -0200

    Update documentation

--- id3v2/errors.go
+++ id3v2/errors.go
@@ -3,17 +3,17 @@ package id3v2
 import "errors"
 
 var (
-	// ErrInvalidSize is the error returned by ParseHeader when
-	// it encounters a invalid zero bit
+	// ErrInvalidSize is the error returned when the entire size
+	// of ID3 header cannot be measured
 	ErrInvalidSize = errors.New("invalid size")
-	// ErrHeaderNotFound is the error returned when ParseHeader
-	// cannot find ID3 header mark
+	// ErrHeaderNotFound is the error returned when ID3 header
+	// mark was not found
 	ErrHeaderNotFound = errors.New("header not found")
-	// ErrUnrecognizedVersion is the error returned by ParseHeader
-	// when the major version of ID3v2 is not recognized by this
-	// library or even is a documented version by ID3v2 standard
+	// ErrUnrecognizedVersion is the error returned when the major
+	// version of ID3v2 is not recognized by this library or even
+	// it's a documented version by the ID3v2 standard
 	ErrUnrecognizedVersion = errors.New("unrecognized version")
-	// ErrUnsupportedVersion is the error returned by Parser#Parse
-	// when there's no capturer for the given ID3v2 major version
+	// ErrUnsupportedVersion is the error returned when there's no
+	// frame capturer for the given ID3v2 major version
 	ErrUnsupportedVersion = errors.New("unsupported version")
 )
