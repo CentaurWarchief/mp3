6dbc5a65835f22566bd04aac2fd62c2a0f86d0a0
Andrey <andreykvital@gmail.com>
2015-12-27 14:18:09 -0200

    ID3v2CapturedFrame#AsReadable

--- id3v2/frame/id3v2capturedframe.go
+++ id3v2/frame/id3v2capturedframe.go
@@ -1,11 +1,30 @@
 package frame
 
+import "io"
+
 type ID3v2CapturedFrame struct {
 	Frame    string
 	Size     uint64
 	Position int
 }
 
+// Returns the frame identifier
 func (f ID3v2CapturedFrame) ID() string {
 	return f.Frame
+}
+
+// Turns the captured frame in a readable frame (ID3v2ReadableFrame)
+func (f ID3v2CapturedFrame) AsReadable(
+	r io.ReaderAt,
+	u func(body []byte) interface{},
+) ID3v2ReadableFrame {
+	return NewID3v2ReadableFrame(f.Frame, func() interface{} {
+		body := make([]byte, f.Size)
+
+		if _, err := r.ReadAt(body, int64(f.Position)); err != nil {
+			return nil
+		}
+
+		return u(body)
+	})
 }
